717421d282ccfc6380d39198776f82c0575adc9e
your name <your_email@youremail.com>
2018-06-13 21:13:47 +0800

    default

--- src/main/java/com/imooc/GirlApplication.java
+++ src/main/java/com/imooc/GirlApplication.java
@@ -1,12 +1,12 @@
-package com.example.demo;
+package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class GirlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GirlApplication.class, args);
 	}
 }
